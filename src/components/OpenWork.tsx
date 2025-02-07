6c7c754de3a16b276b6ec4dcc528673928bb8a34
xakxa01 <Carlosxaviergomez@outlook.com>
2025-02-06 20:59:46 -0400

    fix: update background color of OpenWork component to shamrock-500

--- src/components/OpenWork.tsx
+++ src/components/OpenWork.tsx
@@ -1,5 +1,5 @@
 const OpenWork = () => (
-	<p className='bg-verde w-4/12 text-center rounded-lg text-2xl py-2 font-bold text-bright-gray-950 m-4 animate-pulse md:text-base'>
+	<p className='bg-shamrock-500 w-4/12 text-center rounded-lg text-2xl py-2 font-bold text-bright-gray-950 m-4 animate-pulse md:text-base'>
 		Open to work
 	</p>
 )
